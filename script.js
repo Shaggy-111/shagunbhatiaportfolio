7d2686f3257cbcf298e9323d2d9bbb40b4f16e83
Shagun Bhatia <shagunbhatia98@gmail.com>
2026-02-16 21:42:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll for links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Simple fade-in on load
+window.addEventListener("load", () => {
+  document.body.style.opacity = "1";
+});
